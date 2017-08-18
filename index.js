d553dbaee90f9910433fda935da89a34ed57ac6e
afghl <airport.0930@gmail.com>
2017-08-18 20:41:24 +0800

    add js index helloworld

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 import 'babel-polyfill'
 import './sass/main.scss'
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import App from './containers/App'
-import configureStore from './store/configureStore'
 
-const store = configureStore()
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+const t = () => {
+  return 'a'
+}
+
+console.log(t())
